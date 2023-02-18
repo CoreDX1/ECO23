1a1bc762337a3266fdb240520e33b88de2e711cc
CoreDX1 <chrismaquis@gmail.com>
2023-02-18 19:11:06 -0300

    Tablas nuevas

--- SQLQuery/TablasC.sql
+++ SQLQuery/TablasC.sql
@@ -40,16 +40,55 @@ alter table user_location drop constraint localidad_id_provincia_fkey;
 -- Como agregar una foreing key
 alter table user_location add foreign key(id_province) references province(id_province) on delete cascade;
 
-create table USER_PROFILE
-(
- 	ID_USER_PROFILE SMALLSERIAL,
- 	ID_USER INT not null,
- 	ID_LOCATION smallint not null,
- 	USER_PASSWORD VARCHAR(10) not null,
- 	EMAIL VARCHAR(30) not null,
- 	CREATION_DATE TIMESTAMP not null,
- 	primary key(ID_USER_PROFILE),
- 	foreign key(ID_USER) references USER_ECO(ID_USER) on delete cascade,
-	foreign key(ID_LOCATION) references USER_LOCATION(id_location) on delete cascade
-);
+-- Cambiar el type
+ALTER TABLE user_profile ALTER COLUMN id_user TYPE smallint;
+
+-- Todas la provincias
+INSERT INTO province (name)
+VALUES ('BUENOS AIRES'),
+       ('CATAMARCA'),
+       ('CHACO'),
+       ('CHUBUT'),
+       ('CABA'),
+       ('CÓRDOBA'),
+       ('CORRIENTES'),
+       ('ENTRE RÍOS'),
+       ('FORMOSA'),
+       ('JUJUY'),
+       ('LA PAMPA'),
+       ('LA RIOJA'),
+       ('MENDOZA'),
+       ('MISIONES'),
+       ('NEUQUÉN'),
+       ('RÍO NEGRO'),
+       ('SALTA'),
+       ('SAN JUAN'),
+       ('SAN LUIS'),
+       ('SANTA CRUZ'),
+       ('SANTA FE'),
+       ('SANTIAGO DEL ESTERO'),
+       ('TIERRA DEL FUEGO'),
+       ('TUCUMÁN');
+
+insert into user_status(status)
+values('ADMIN'),('CLIENT'),('EMPLOYEE');
+
+insert into user_permission(id_user_eco,id_user_status)
+values(11,2)
+
+insert into user_eco(name,paternal_last_name,maternal_last_name,phone)
+values('CAMILO','ALTARAZ','BEGON',1152192936);
+
+insert 
+	into 
+	user_location (street, height, id_province)
+	values('TRAVESIA ABREU', 295,17);
+
+insert 
+	into
+	user_profile(id_user,id_location,user_password,creation_date,email)
+	values(11,1,'index','2023-01-08 04:05:06','camilo@gmail.com')
+
+select * from user_profile;
+
 
