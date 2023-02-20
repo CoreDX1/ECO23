640cc0cec462d00b3d99de1377809cbc6fbf2604
CoreDX1 <chrismaquis@gmail.com>
2023-02-20 13:19:44 -0300

    La respuesta en json

---
+++ POS/src/POS.Application/Commons/Base/BaseResponse.cs
@@ -0,0 +1,8 @@
+namespace POS.Application.Commons.Base;
+
+public class BaseResponse<T>
+{
+    public bool IsSuccess { get; set; }
+    public string? Message { get; set; }
+    public T? Data { get; set; }
+}
