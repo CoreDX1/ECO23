15190efe006ddb424683335fb24034976fc87a13
CoreDX1 <chrismaquis@gmail.com>
2023-02-20 13:19:29 -0300

    Injectadod las dependencias de App y Infrea

--- POS/src/POS.Api/Program.cs
+++ POS/src/POS.Api/Program.cs
@@ -1,8 +1,15 @@
+using POS.Application.Extensions;
+using POS.Infrastructure.Persistence.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+var Configuration = builder.Configuration;
 builder.Services.AddControllers();
+
+builder.Services.AddInjectionInfrastructure(Configuration);
+builder.Services.AddInjectionApplication(Configuration);
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
