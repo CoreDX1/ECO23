d442d9f715b72e7c4f10b1eb1f9f060658fe7d85
CoreDX1 <chrismaquis@gmail.com>
2023-02-20 13:21:40 -0300

    Creado la interface de UserEco

---
+++ POS/src/POS.Application/Interfaces/IUserEcoApplication.cs
@@ -0,0 +1,9 @@
+using POS.Application.Commons.Base;
+using POS.Domain.Entities;
+
+namespace POS.Application.Interfaces;
+
+public interface IUserEcoApplication
+{
+    Task<BaseResponse<IEnumerable<UserEco>>> ListSelectUser();
+}
