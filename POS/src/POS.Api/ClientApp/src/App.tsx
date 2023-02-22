762078120994c0c607f1d63b0ec317be64d06012
CoreDX1 <chrismaquis@gmail.com>
2023-02-22 14:26:50 -0300

    Save

--- POS/src/POS.Api/ClientApp/src/App.tsx
+++ POS/src/POS.Api/ClientApp/src/App.tsx
@@ -1,12 +1,21 @@
-import { useState } from 'react'
+import Usuario from "./Usuarios.json"
 import './App.css'
-
 function App() {
+  function UsuarioList(){
+    const usuario = Usuario.data.find(u => u.idUser === 11)
+    return (
+      <div>
+        <h1>{usuario?.name}</h1>
+        <h2>{usuario?.cellPhone}</h2>
+        <p>Altura y Direccion</p>
+        <p>{usuario?.userProfiles.localidad.Number}</p>
+        <p>{usuario?.userProfiles.localidad.Street}</p>
+      </div>
+    )
+  }
   return (
-    <div className="App">
-      <h1>
-        Hola mundo
-      </h1>
+    <div>
+        {UsuarioList()}
     </div>
   )
 }
--- POS/src/POS.Infrastructure/Persistence/Repositories/UserEcoRepository.cs
+++ POS/src/POS.Infrastructure/Persistence/Repositories/UserEcoRepository.cs
@@ -1,5 +1,4 @@
 using Microsoft.EntityFrameworkCore;
-using POS.Domain.Entities;
 using POS.Infrastructure.Persistence.DataContext;
 using POS.Infrastructure.Persistence.Interfaces;
 
