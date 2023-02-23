fabf530a457701cf94c37ac7bcaf37261abdb772
CoreDX1 <chrismaquis@gmail.com>
2023-02-23 19:45:05 -0300

    JsonIgnore cuando el resultado sea null

--- POS/src/POS.Application/Commons/Base/BaseResponse.cs
+++ POS/src/POS.Application/Commons/Base/BaseResponse.cs
@@ -1,8 +1,15 @@
+using System.Text.Json.Serialization;
+
 namespace POS.Application.Commons.Base;
 
 public class BaseResponse<T>
 {
     public bool IsSuccess { get; set; }
     public string? Message { get; set; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public T? Data { get; set; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public IEnumerable<string>? Errors { get; set; }
 }
