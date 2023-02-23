09f67467c0091c795d29040d6444398e9a57e611
CoreDX1 <chrismaquis@gmail.com>
2023-02-23 19:48:31 -0300

    Creado el num para las provincias

---
+++ POS/src/POS.Utilities/Static/ProvinceEnum.cs
@@ -0,0 +1,29 @@
+namespace POS.Utilities.Static;
+
+public enum ProvinceEnum
+{
+    BUENOS_AIRES = 1,
+    CATAMARCA = 2,
+    CHACO = 3,
+    CHUBUT = 4,
+    CABA = 5,
+    CORDOBA = 6,
+    CORRIENTES = 7,
+    ENTRE_RIOS = 8,
+    FORMOSA = 9,
+    JUJUY = 10,
+    LA_PAMPA = 11,
+    LA_RIOJA = 12,
+    MENDOZA = 13,
+    MISIONES = 14,
+    NEUQUEN = 15,
+    RIO_NEGRO = 16,
+    SALTA = 17,
+    SAN_JUAN = 18,
+    SAN_LUIS = 19,
+    SANTA_CRUZ = 20,
+    SANTA_FE = 21,
+    SANTIAGO_DEL_ESTERO = 22,
+    TIERRA_DEL_FUEGO = 23,
+    TUCUMAN = 24
+}
