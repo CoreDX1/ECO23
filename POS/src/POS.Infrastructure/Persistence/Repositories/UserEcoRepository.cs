d0f884112f52b2855db3388c2019dd8864ada1c2
CoreDX1 <chrismaquis@gmail.com>
2023-02-23 19:48:06 -0300

    El tipo anomino no esta agradable para buscar id

--- POS/src/POS.Infrastructure/Persistence/Repositories/UserEcoRepository.cs
+++ POS/src/POS.Infrastructure/Persistence/Repositories/UserEcoRepository.cs
@@ -14,33 +14,79 @@ public class UserEcoRepository : IUserEcoRepository
         _context = context;
     }
 
-    public async Task<dynamic> ListSelectUser()
+    public async Task<IEnumerable<UserEco>> ListSelectUser()
     {
-        var fusion = await (
+        IEnumerable<UserEco> fusion = await (
             from user in _context.UserEco
             join location in _context.UserLocation
                 on user.UserProfile.IdLocation equals location.IdLocation
             join profile in _context.UserProfile on user.IdUser equals profile.IdUser
             join province in _context.Province on location.IdProvince equals province.IdProvince
-            select new
+            select new UserEco
             {
-                user.IdUser,
-                user.Name,
-                user.PaternalLastName,
-                user.MaternalLastName,
-                user.CellPhone,
-                user.UserPermissions,
-                UserProfile = new
+                IdUser = user.IdUser,
+                Name = user.Name,
+                PaternalLastName = user.PaternalLastName,
+                MaternalLastName = user.MaternalLastName,
+                CellPhone = user.CellPhone,
+                UserPermissions = user.UserPermissions,
+                UserProfile = new UserProfile
                 {
-                    profile.IdUser,
-                    profile.Email,
-                    profile.UserPassword,
-                    profile.CreationDate,
-                    location = new { location.Street, Province = location.IdProvinceNavigation }
+                    IdUserProfile = profile.IdUserProfile,
+                    IdLocation = location.IdLocation,
+                    IdUser = profile.IdUser,
+                    Email = profile.Email,
+                    UserPassword = profile.UserPassword,
+                    CreationDate = profile.CreationDate,
+                    IdLocationNavigation = new UserLocation
+                    {
+                        Street = location.Street,
+                        HouseNumber = location.HouseNumber,
+                        IdProvince = location.IdProvince,
+                        IdProvinceNavigation = new Province
+                        {
+                            IdProvince = location.IdProvince,
+                            Name = province.Name
+                        }
+                    }
                 }
             }
         ).AsNoTracking().ToListAsync();
 
         return fusion;
+    }
+
+    public async Task<UserEco> UserById(int id)
+    {
+        IEnumerable<UserEco> list = await ListSelectUser();
+        UserEco? user = list.SingleOrDefault(x => x.IdUser == id);
+        return user!;
+    }
+
+    public async Task<bool> CreateUserEco(UserEco userEco)
+    {
+        await isValidateEmail(userEco);
+        await _context.UserEco.AddAsync(userEco);
+        await _context.SaveChangesAsync();
+        return true;
+    }
+
+    private async Task<bool> isValidateEmail(UserEco data)
+    {
+        bool user = await _context.UserEco
+            .Where(x => x.UserProfile.Email.Equals(data.UserProfile.Email))
+            .AnyAsync();
+
+        return user;
+    }
+
+    public Task<bool> UpdateUserEco(UserEco userEco)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> DeleteUserEco(int id)
+    {
+        throw new NotImplementedException();
     }
 }
