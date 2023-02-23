18c3dbace5ec3531b2311ef31bd0c7ed156f7da7
CoreDX1 <chrismaquis@gmail.com>
2023-02-23 19:47:21 -0300

    Creando la interface de UserEco para Infastructure

--- POS/src/POS.Infrastructure/Persistence/Interfaces/IUserEcoRepository.cs
+++ POS/src/POS.Infrastructure/Persistence/Interfaces/IUserEcoRepository.cs
@@ -4,5 +4,9 @@ namespace POS.Infrastructure.Persistence.Interfaces;
 
 public interface IUserEcoRepository
 {
-    public Task<dynamic> ListSelectUser();
+    public Task<IEnumerable<UserEco>> ListSelectUser();
+    public Task<UserEco> UserById(int id);
+    public Task<bool> UpdateUserEco(UserEco userEco);
+    public Task<bool> DeleteUserEco(int id);
+    public Task<bool> CreateUserEco(UserEco userEco);
 }
