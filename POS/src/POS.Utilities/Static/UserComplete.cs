be59f822b70f642ddd608749261d8b4287c4823d
CoreDX1 <chrismaquis@gmail.com>
2023-02-24 19:52:30 -0300

    Ignorado Json

--- POS/src/POS.Utilities/Static/UserComplete.cs
+++ POS/src/POS.Utilities/Static/UserComplete.cs
@@ -1,21 +1,28 @@
+using System.Text.Json.Serialization;
+
 namespace POS.Utilities.Static;
 
 public class UserComplete
 {
     // INFO : Profile Table //
     public string? UserPassword { get; set; }
+    public string? ReplyPassword { get; set; }
     public string? Email { get; set; }
     public DateTime CreationDate { get; set; }
 
     // INFO : User Table //
+    [JsonIgnore]
     public short IdUser { get; set; }
     public string? Name { get; set; }
     public string? PaternalLastName { get; set; }
     public string? MaternalLastName { get; set; }
     public string? CellPhone { get; set; }
+
+    [JsonIgnore]
     public short IdUserProfile { get; set; }
 
     // INFO : Localidad Table //
+    [JsonIgnore]
     public short IdLocation { get; set; }
     public string? Street { get; set; }
     public int HouseNumber { get; set; }
