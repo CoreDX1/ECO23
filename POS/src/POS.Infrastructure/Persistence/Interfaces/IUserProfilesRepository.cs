9b52d602931a0ea57845e499d36359e69f03ce7d
CoreDX1 <chrismaquis@gmail.com>
2023-02-24 15:57:19 -0300

    Borrado

--- POS/src/POS.Infrastructure/Persistence/Interfaces/IUserProfilesRepository.cs
+++
@@ -1,8 +0,0 @@
-using POS.Domain.Entities;
-
-namespace POS.Infrastructure.Persistence.Interfaces;
-
-public interface IUserProfilesRepository
-{
-    Task<IEnumerable<UserProfile>> ListSelectUserProfile();
-}
