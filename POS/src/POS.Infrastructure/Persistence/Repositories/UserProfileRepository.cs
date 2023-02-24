74fd20d55057a1e67a059afdb7589441bd2a3693
CoreDX1 <chrismaquis@gmail.com>
2023-02-24 19:51:53 -0300

    Usado Tuplas

--- POS/src/POS.Infrastructure/Persistence/Repositories/UserProfileRepository.cs
+++ POS/src/POS.Infrastructure/Persistence/Repositories/UserProfileRepository.cs
@@ -16,30 +16,43 @@ public class UserProfileRepository : IUserProfileRepository
 
     public async Task<bool> CreateUserProfile(UserComplete userProfile)
     {
-        short user = (
-            from n in _context.UserEco
-            where n.Name == userProfile.Name && n.CellPhone == userProfile.CellPhone
-            select n.IdUser
-        ).FirstOrDefault();
-
-        short location = (
-            from n in _context.UserLocation
-            where n.Street == userProfile.Street && n.HouseNumber == userProfile.HouseNumber
-            select n.IdLocation
-        ).FirstOrDefault();
-        if (user > 0 && location > 0)
+        if (!ValidateUser(userProfile))
             return false;
 
+        var (userId, locationId) = GetIdUserLocation(userProfile);
         var profile = new UserProfile
         {
             Email = userProfile.Email!,
             UserPassword = userProfile.UserPassword!,
             CreationDate = DateTime.Now,
-            IdUser = user,
-            IdLocation = location,
+            IdUser = userId,
+            IdLocation = locationId,
         };
         await _context.UserProfile.AddAsync(profile);
         var result = await _context.SaveChangesAsync();
         return result > 0;
+    }
+
+    private (short, short) GetIdUserLocation(UserComplete user)
+    {
+        short idUser = (
+            from n in _context.UserEco
+            where n.Name == user.Name && n.CellPhone == user.CellPhone
+            select n.IdUser
+        ).FirstOrDefault();
+
+        short idLocation = (
+            from n in _context.UserLocation
+            where n.Street == user.Street && n.HouseNumber == user.HouseNumber
+            select n.IdLocation
+        ).FirstOrDefault();
+        return (idUser, idLocation);
+    }
+
+    private bool ValidateUser(UserComplete user)
+    {
+        if (user.UserPassword != user.ReplyPassword)
+            return false;
+        return true;
     }
 }
