d74f302c095391d803e67d764cc993a0291b2b3d
CoreDX1 <chrismaquis@gmail.com>
2023-02-24 19:51:40 -0300

    Agregando las reglas

---
+++ POS/src/POS.Application/Validators/UserValidatorRules.cs
@@ -0,0 +1,25 @@
+using FluentValidation;
+using POS.Utilities.Static;
+
+namespace POS.Application.Validators;
+
+public class UserValidatorRules : AbstractValidator<UserComplete>
+{
+    public UserValidatorRules()
+    {
+        RuleFor<string?>(x => x.UserPassword)
+            .Equal(x => x.ReplyPassword)
+            .WithMessage("Las contraseñas no coinciden")
+            .NotNull()
+            .NotEmpty()
+            .WithMessage("La contraseña no puede estar vacia");
+        RuleFor<string?>(x => x.Email)
+            .EmailAddress()
+            .WithMessage("El email no es valido")
+            .NotNull()
+            .NotEmpty()
+            .WithMessage("El email no puede estar vacio");
+        ;
+        RuleFor(x => x.IdProvince).IsInEnum().WithMessage("El id de provincia no es valido");
+    }
+}
