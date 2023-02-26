20b65356260888ec02ced69e87e9b79a7b2c8981
CoreDX1 <chrismaquis@gmail.com>
2023-02-26 13:47:30 -0300

    validacion de email

--- POS/src/POS.Infrastructure/Persistence/Repositories/UserProfileRepository.cs
+++ POS/src/POS.Infrastructure/Persistence/Repositories/UserProfileRepository.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 using POS.Domain.Entities;
 using POS.Infrastructure.Persistence.DataContext;
 using POS.Infrastructure.Persistence.Interfaces;
@@ -16,8 +17,16 @@ public class UserProfileRepository : IUserProfileRepository
 
     public async Task<bool> CreateUserProfile(UserProfile addProfile)
     {
+        bool isEmailValidate = await isValidateEmail(addProfile.Email);
+        if (isEmailValidate)
+            return false;
         await _context.UserProfile.AddAsync(addProfile);
         var result = await _context.SaveChangesAsync();
         return result > 0;
+    }
+
+    private async Task<bool> isValidateEmail(string email)
+    {
+        return await _context.UserProfile.AnyAsync(x => x.Email.Equals(email));
     }
 }
