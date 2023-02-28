428803e868a92d726fd876f6bc91fac2f2764aa5
CoreDX1 <chrismaquis@gmail.com>
2023-02-28 10:47:58 -0300

    Mapeando Location

--- POS/src/POS.Application/Mappers/UserEcoMappingsProfile.cs
+++ POS/src/POS.Application/Mappers/UserEcoMappingsProfile.cs
@@ -1,5 +1,6 @@
 using AutoMapper;
 using POS.Application.DTO.Request;
+using POS.Application.DTO.Response.Location;
 using POS.Domain.Entities;
 
 namespace POS.Application.Mappers;
@@ -12,5 +13,11 @@ public class UserEcoMappingsProfile : Profile
         CreateMap<UserEcoRequestDto, UserLocation>()
             .ForMember(dest => dest.IdProvince, opt => opt.MapFrom(src => (short)src.IdProvince));
         CreateMap<UserEcoRequestDto, UserProfile>();
+
+        // INFO : Location
+        // * Response
+        CreateMap<UserLocation, LocationResponseDto>();
+
+        // INFO : Profile
     }
 }
