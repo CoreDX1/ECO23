d2dd20c9de5159a0946caed7d2b960cbb2bf921f
CoreDX1 <chrismaquis@gmail.com>
2023-02-28 10:52:34 -0300

    Agregando el metodo ValidteEmail

--- POS/src/POS.Infrastructure/Persistence/Interfaces/IUserProfileRepository.cs
+++ POS/src/POS.Infrastructure/Persistence/Interfaces/IUserProfileRepository.cs
@@ -1,9 +1,9 @@
 using POS.Domain.Entities;
-using POS.Utilities.Static;
 
 namespace POS.Infrastructure.Persistence.Interfaces;
 
 public interface IUserProfileRepository
 {
-    Task<bool> CreateUserProfile(UserProfile addProfile);
+    Task<bool> CreateUserProfile(UserProfile addProfile, short idLocation, short idUser);
+    Task<UserProfile> isValidateEmail(string email);
 }
