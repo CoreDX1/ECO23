1827d084c162210c743cd28379c04ac0f587901a
CoreDX1 <chrismaquis@gmail.com>
2023-02-28 14:42:14 -0300

    Usando el GenericoRepository

--- POS/src/POS.Infrastructure/Persistence/Repositories/UserLocationRepository.cs
+++ POS/src/POS.Infrastructure/Persistence/Repositories/UserLocationRepository.cs
@@ -1,23 +1,12 @@
 using POS.Domain.Entities;
 using POS.Infrastructure.Persistence.DataContext;
 using POS.Infrastructure.Persistence.Interfaces;
+using POS.Infrastructure.Persistence.Repositories.GenericRepository;
 using POS.Utilities.Static;
 
 namespace POS.Infrastructure.Persistence.Repositories;
 
-public class UserLocationRepository : IUserLocationRepository
+public class UserLocationRepository : GenericRepository<UserLocation>, IUserLocationRepository
 {
-    private readonly Eco23Context _context;
-
-    public UserLocationRepository(Eco23Context context)
-    {
-        _context = context;
-    }
-
-    public async Task<UserLocation> CreateUserLocation(UserLocation addLocation)
-    {
-        await _context.UserLocation.AddAsync(addLocation);
-        var result = await _context.SaveChangesAsync();
-        return addLocation;
-    }
+    public UserLocationRepository(Eco23Context context) : base(context) { }
 }
--- POS/src/POS.Infrastructure/Persistence/Repositories/UserProfileRepository.cs
+++ POS/src/POS.Infrastructure/Persistence/Repositories/UserProfileRepository.cs
@@ -2,37 +2,11 @@ using Microsoft.EntityFrameworkCore;
 using POS.Domain.Entities;
 using POS.Infrastructure.Persistence.DataContext;
 using POS.Infrastructure.Persistence.Interfaces;
+using POS.Infrastructure.Persistence.Repositories.GenericRepository;
 
 namespace POS.Infrastructure.Persistence.Repositories;
 
-public class UserProfileRepository : IUserProfileRepository
+public class UserProfileRepository : GenericRepository<UserProfile>, IUserProfileRepository
 {
-    private readonly Eco23Context _context;
-
-    public UserProfileRepository(Eco23Context context)
-    {
-        _context = context;
-    }
-
-    public async Task<bool> CreateUserProfile(
-        UserProfile addProfile,
-        short idLocation,
-        short idUser
-    )
-    {
-        addProfile.IdLocation = idLocation;
-        addProfile.IdUser = idUser;
-        addProfile.CreationDate = DateTime.Now;
-        await _context.UserProfile.AddAsync(addProfile);
-        var result = await _context.SaveChangesAsync();
-        return result > 0;
-    }
-
-    public async Task<UserProfile> isValidateEmail(string email)
-    {
-        var isMail = await _context.UserProfile
-            .Where(x => x.Email.Equals(email))
-            .SingleOrDefaultAsync();
-        return isMail!;
-    }
+    public UserProfileRepository(Eco23Context context) : base(context) { }
 }
